e8b5d8c648103f4ccd26ae20a8b3ca63dc0cfe69
Russell Ng <russelldaniel.ng@gmail.com>
2016-01-21 19:01:20 +1300

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var db = {
-  dir: "C:/Users/GFX/My Documents/RUSSELL/DEVELOPMENT/NewsHub-Graphics/src/test-db/test-db.db"
+  dir: "C:/Users/GFX/Documents/RUSSELL/DEVELOPMENT/NewsHub-Graphics/src/test-db/test-db.db"
 }
 
 console.log('db.js loaded');

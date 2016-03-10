830e8749a0ff8c3b9a71c1fb0d22b70f61c45c53
Russell Ng <russelldaniel.ng@gmail.com>
2016-03-10 18:31:51 +1300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var nasPath = "C:/Users/GFX/Documents/RUSSELL/DEVELOPMENT/NewsHub-Graphics/apps/
 
 var db = {
   dir: nasPath + "job-queue/jobName.job",
-  userImgs: nasPath + "users/user-imgs"
+  userImgs: nasPath + "users/user-imgs",
   jobTypeConfig: nasPath + "jobIO/jobConfig/jobTypeConfig.js"
 }
 

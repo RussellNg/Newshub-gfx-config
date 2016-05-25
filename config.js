1545ae714654fcbce1c7eca29efac0a30bf29871
Russell Ng <russelldaniel.ng@gmail.com>
2016-05-25 17:17:50 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 {
-  nasPath: "C:/Users/GFX/Documents/RUSSELL/DEVELOPMENT/NewsHub-Graphics/apps/_fakeNAS/",
-  userImgs: this.nasPath + "users/user-imgs",
-  jobTypeConfig: this.nasPath + "_config/jobIO/jobConfig/jobTypeConfig.js",
-  loaded: true
+  "nasPath": "C:/Users/GFX/Documents/RUSSELL/DEVELOPMENT/NewsHub-Graphics/apps/_fakeNAS/",
+  "userImgs": this.nasPath + "users/user-imgs",
+  "jobTypeConfig": this.nasPath + "_config/jobIO/jobConfig/jobTypeConfig.js",
+  "loaded": true
 }

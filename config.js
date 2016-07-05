981009feaeac5cea398dd941f6925289e5d9db91
Russell Ng <russelldaniel.ng@gmail.com>
2016-07-05 17:25:19 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
-  "nasPath": "//10.13.1.53/nas/gfx/newshub-gfx",
+  "nasPath": "//10.13.1.53/nas/gfx/newshub-gfx/",
   "userImgs": '',
   "jobTypeConfig": '',
   "loaded": true
 }
 
-config.userImgs = config.nasPath + "users/user-imgs";
+config.userImgs = config.nasPath + "users/user-imgs/";
 config.jobTypeConfig = config.nasPath + "_config/jobIO/jobConfig/jobTypeConfig.js";
 
 console.log('config.js loaded.');
